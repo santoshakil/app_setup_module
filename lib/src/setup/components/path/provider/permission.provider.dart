0d3fe16aee6dfa6b8d52b6fd016a6bd249638afd
kakon007 <kakon443@gmail.com>
2022-11-28 16:14:21 +0600

    wip

---
+++ lib/src/setup/components/path/provider/permission.provider.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/foundation.dart' show debugPrint;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+typedef _Notifier
+    = AsyncNotifierProviderFamily<PermissionProvider, bool, Directory?>;
+
+final permissionProvider = _Notifier(PermissionProvider.new);
+
+class PermissionProvider extends FamilyAsyncNotifier<bool, Directory?> {
+  @override
+  FutureOr<bool> build(Directory? arg) async {
+    if (arg == null) return false;
+
+    try {
+      await arg.createTemp();
+      return true;
+    } on Exception catch (e) {
+      debugPrint('$e');
+      return false;
+    }
+  }
+}
--- lib/src/setup/components/path/view/path.view.dart
+++ lib/src/setup/components/path/view/path.view.dart
@@ -2,31 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../provider/path.provider.dart';
+import '../provider/permission.provider.dart';
 import 'db.selector.dart';
 
-class PathView extends ConsumerWidget {
+class PathView extends StatelessWidget {
   const PathView({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         const DBSelectorTile(),
         Row(
           mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-              child: ElevatedButton(
-                child: const Text('Next'),
-                onPressed: () async =>
-                    await ref.read(dbPathProvider.notifier).create(),
-              ),
-            ),
+          children: const [
+            _Next(),
           ],
         ),
       ],
+    );
+  }
+}
+
+class _Next extends ConsumerWidget {
+  const _Next();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final path = ref.watch(dbPathProvider).value;
+    if (path == null) return const SizedBox.shrink();
+    final permitted = ref.watch(permissionProvider(path)).value ?? false;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: ElevatedButton(
+        onPressed: permitted
+            ? () async => await ref.read(dbPathProvider.notifier).create()
+            : null,
+        child: const Text('Next'),
+      ),
     );
   }
 }
